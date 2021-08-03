2697aa2884c2bfc28211c6adfd07118ea05e40f1
Irine Tuzova <alexandrinius@outlook.com>
2021-08-03 15:21:07 +0300

    Revision

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  void findMaxIndex() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
 }
